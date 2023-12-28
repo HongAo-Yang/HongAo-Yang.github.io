8caec679e15ba8f93ec56605259eae7fd542f3cf
Yang HongAo <yha21@mails.tsinghua.edu.cn>
2023-12-28 21:41:31 +0800

    feat(home): mobile menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppShell, AppBar, AppRail, AppRailAnchor } from '@skeletonlabs/skeleton';
 	import { TabGroup, Tab, TabAnchor } from '@skeletonlabs/skeleton';
 	import { LightSwitch } from '@skeletonlabs/skeleton';
 
@@ -25,29 +25,125 @@
 	import { page } from '$app/stores';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
+	// Icon
+	import Icon from '$lib/Icon.svelte';
+
 	// Font Awesome
 	import '@fortawesome/fontawesome-free/css/fontawesome.css';
 	import '@fortawesome/fontawesome-free/css/brands.css';
 	import '@fortawesome/fontawesome-free/css/solid.css';
+
+	// Side Drawer
+	import { initializeStores } from '@skeletonlabs/skeleton';
+	initializeStores();
+	import { getDrawerStore, Drawer } from '@skeletonlabs/skeleton';
+	const drawerStore = getDrawerStore();
+	function drawerOpen(): void {
+		drawerStore.open();
+	}
 </script>
+
+<!-- Drawer -->
+<Drawer width="auto">
+	<AppRail>
+		<AppRailAnchor href="/" on:click={drawerStore.close}>
+			<svelte:fragment slot="lead"><i class="fa-solid fa-home text-2xl" /></svelte:fragment>
+			<span>Home</span>
+		</AppRailAnchor>
+		<AppRailAnchor href="/about" on:click={drawerStore.close}>
+			<svelte:fragment slot="lead"><i class="fa-solid fa-user text-2xl" /></svelte:fragment>
+			<span>About</span>
+		</AppRailAnchor>
+		<AppRailAnchor href="/research" on:click={drawerStore.close}>
+			<svelte:fragment slot="lead"><i class="fa-solid fa-flask text-2xl" /></svelte:fragment>
+			<span>Research</span>
+		</AppRailAnchor>
+		<AppRailAnchor href="/blog" on:click={drawerStore.close}>
+			<svelte:fragment slot="lead"><i class="fa-solid fa-blog text-2xl" /></svelte:fragment>
+			<span>Blog</span>
+		</AppRailAnchor>
+		<AppRailAnchor href="/photo" on:click={drawerStore.close}>
+			<svelte:fragment slot="lead"><i class="fa-solid fa-camera text-2xl" /></svelte:fragment>
+			<span>Photo</span>
+		</AppRailAnchor>
+	</AppRail>
+</Drawer>
+
+<!-- <div class="btn-group-vertical">
+	<button class="btn" on:click={drawerStore.close}>
+		<i class="fa-solid fa-bars text-xl" />
+	</button>
+	<a href="/" class="btn btn-primary">
+		<span>Home</span>
+	</a>
+	<a href="/about" class="btn btn-primary">
+		<span>About</span>
+	</a>
+</div>
+
+		<button class="btn flex flex-col" on:click={drawerStore.close}>
+			<i class="fa-solid fa-bars text-xl" />
+		</button>
+		<a href="/" class="btn btn-primary">
+			<span>Home</span>
+		</a>
+		<a href="/about" class="btn btn-primary">
+			<span>About</span>
+		</a>
+		<a href="/research" class="btn btn-primary">
+			<span>Research</span>
+		</a>
+		<a href="/blog" class="btn btn-primary">
+			<span>Blog</span>
+		</a>
+		<a href="/photo" class="btn btn-primary">
+			<span>Photo</span>
+		</a>
+	</div>
+</Drawer> -->
 
 <!-- App Shell -->
 <AppShell>
 	<svelte:fragment slot="header">
-		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+		<AppBar class="lg:!hidden">
 			<svelte:fragment slot="lead">
-				<a href="/" class="lg:!ml-0 w-[48px] lg:w-auto overflow-hidden">
-					<svg
-						class="fill-token"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 178 160"
-						height="32px"
-					>
-						<polygon
-							points="138 0 138 80 78 80 116 0 76 0 58 37.89 40 0 0 0 38 80 38 160 78 160 78 100 138 100 138 160 178 160 178 0 138 0"
-						/>
-					</svg>
+				<div class="flex items-center space-x-4">
+					<!-- menu -->
+					<button on:click={drawerOpen} class="btn-icon btn-icon-sm">
+						<i class="fa-solid fa-bars text-xl" />
+					</button>
+					<!-- logo -->
+					<a href="/" class="lg:!ml-0 w-[48px] lg:w-auto overflow-hidden">
+						<Icon />
+					</a>
+				</div>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a href="https://github.com/HongAo-Yang" target="_blank" rel="noreferrer">
+					<i class="fa-brands text-[24px] fa-github" />
 				</a>
+				<a href="https://www.researchgate.net/profile/Hongao-Yang" target="_blank" rel="noreferrer">
+					<i class="fa-brands text-[24px] fa-researchgate" />
+				</a>
+				<a href="mailto:yha21@tsinghua.edu.cn" target="_blank" rel="noreferrer">
+					<i class="fa-solid text-[24px] fa-envelope" />
+				</a>
+				<LightSwitch /></svelte:fragment
+			>
+		</AppBar>
+		<AppBar
+			class="relative hidden lg:block"
+			gridColumns="grid-cols-3"
+			slotDefault="place-self-center"
+			slotTrail="place-content-end"
+		>
+			<svelte:fragment slot="lead">
+				<div class="flex item-center space-x-4">
+					<!-- logo -->
+					<a href="/" class="lg:!ml-0 w-[48px] lg:w-auto overflow-hidden">
+						<Icon />
+					</a>
+				</div>
 			</svelte:fragment>
 			<TabGroup
 				justify="justify-center"
