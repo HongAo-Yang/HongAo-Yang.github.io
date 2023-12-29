b75ebd717a936aec1722393011d04f1775efc33c
Yang HongAo <yha21@mails.tsinghua.edu.cn>
2023-12-29 16:37:36 +0800

    feat(photo): upload some photos

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,1 +1,1 @@
-<h1 class="h1">Under development</h1>
+<h1 class="h1 text-center">Under development</h1>
--- src/routes/photo/+page.svelte
+++ src/routes/photo/+page.svelte
@@ -1,1 +1,13 @@
-<h1 class="h1">Under development</h1>
+<div class="page-container">
+	<div class="columns-2 md:columns-3 lg:columns-4 gap-8 [&>img:not(:first-child)]:mt-8">
+		<img src="/photo/ForbiddenCity.jpg" alt="ForbiddenCity" class="w-full" />
+		<img src="/photo/JiaoLou.jpg" alt="JiaoLou" class="w-full" />
+		<img src="/photo/Cat.jpg" alt="Cat" class="w-full" />
+		<img src="/photo/QingDao.jpg" alt="QingDao" class="w-full" />
+		<img src="/photo/Yuyuantan.jpg" alt="Yuyuantan" class="w-full" />
+		<img src="/photo/MeteorShower.jpg" alt="MeteorShower" class="w-full" />
+		<img src="/photo/Train.jpg" alt="Train" class="w-full" />
+        <img src="/photo/StarTrails.jpg" alt="StarTrails" class="w-full" />
+	</div>
+    <h1 class="h3 text-center">Under development</h1>
+</div>
